2133949a64dbc63ebc20385961dbf3860080f0bc
james901 <giacomo__95@hotmail.it>
2022-10-04 16:05:53 +0200

    Checking duplicates and utility file created

--- utils/tools_ntbk.py
+++ utils/tools_ntbk.py
@@ -1,4 +1,5 @@
 import gdown
+from pandas import DataFrame
 
 USERS = "16b7Gbe69-SaVzGc1x3s3s7b8lzHgd5BR"
 TWEETS = "1qYoicySRBbLi9Y8ZytMEi9ee2dIhxxUe"
@@ -7,3 +8,11 @@ TWEETS = "1qYoicySRBbLi9Y8ZytMEi9ee2dIhxxUe"
 def download_dataset():     # Download dataset from Google Drive
     gdown.download(f"https://drive.google.com/uc?id={USERS}", quiet=False)
     gdown.download(f"https://drive.google.com/uc?id={TWEETS}", quiet=False)
+
+
+
+def find_and_remove_duplicates(df: DataFrame):
+    original_lenght = len(df)
+    df = df.drop_duplicates()
+    new_lenght = len(df)
+    return df, original_lenght - new_lenght
